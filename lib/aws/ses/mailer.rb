aec125c55a45c7ffd1aadade3ff334828169cb3b
tabakazu <tkazz.bd1@gmail.com>
2019-08-03 21:41:14 +0900

    move add_options method

--- lib/aws/ses/mailer.rb
+++ lib/aws/ses/mailer.rb
@@ -2,6 +2,18 @@ module Aws
   module Ses
     module Mailer
       autoload :DeliveryMethod, 'aws/ses/mailer/delivery_method'
+
+      class << self
+        def add_options(options = { region: 'us-west-2' })
+          ActiveSupport.on_load :action_mailer do
+            ActionMailer::Base.add_delivery_method(
+              :aws_ses_mailer,
+              Aws::Ses::Mailer::DeliveryMethod,
+              options
+            )
+          end
+        end
+      end
     end
   end
 end
--- lib/aws/ses/mailer/railtie.rb
+++ lib/aws/ses/mailer/railtie.rb
@@ -6,18 +6,6 @@ module Aws
           Aws::Ses::Mailer.add_options
         end
       end
-
-      class << self
-        def add_options(options = { region: 'us-west-2' })
-          ActiveSupport.on_load :action_mailer do
-            ActionMailer::Base.add_delivery_method(
-              :aws_ses_mailer,
-              Aws::Ses::Mailer::DeliveryMethod,
-              options
-            )
-          end
-        end
-      end
     end
   end
 end
